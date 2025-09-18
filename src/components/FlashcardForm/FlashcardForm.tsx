c80dab9b30a210fd7ea4a17072a8f3a29fcc07ab
michellebarrigaIT <michelle.barriga@assuresoft.tech>
2025-09-18 19:22:22 -0400

    feat: add routes

---
+++ src/components/FlashcardForm/FlashcardForm.tsx
@@ -0,0 +1,15 @@
+
+
+function FlashcardForm() {
+  return (
+    <div>
+        <h1>Flashcard Form</h1>
+        <input placeholder="Question" />
+        <input placeholder="Answer" />
+        <input placeholder="Topic" />
+        <button>Save Flashcard</button>
+    </div>
+  )
+}
+
+export default FlashcardForm
---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,12 @@
+import "./NotFound.scss";
+
+function NotFound() {
+  return (
+    <div className="not-found">
+      <h1>404 - Page Not Found </h1>
+      <p>The page you are looking for doesn’t exist.</p>
+    </div>
+  );
+}
+
+export default NotFound
